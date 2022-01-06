bc9e1d1b0f1183798c7e654f6a30efa9547f4b93
yosepjeon <enekelx1@naver.com>
2022-01-06 22:24:19 +0900

    비동기: Emitter

--- ch5-spring-mvc-async/src/main/java/com/yosep/spring/web/controller/ReservationQueryController1.java
+++ ch5-spring-mvc-async/src/main/java/com/yosep/spring/web/controller/ReservationQueryController1.java
@@ -10,29 +10,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
 
 @Controller
-@RequestMapping("/reservation-query")
+@RequestMapping("/reservation-query1")
 public class ReservationQueryController1 {
     private final ReservationService reservationService;
+    private final TaskExecutor taskExecutor;
 
-    public ReservationQueryController1(ReservationService reservationService) {
+    public ReservationQueryController1(ReservationService reservationService, TaskExecutor taskExecutor) {
         this.reservationService = reservationService;
+        this.taskExecutor = taskExecutor;
     }
 
     @GetMapping
-    public void setupForm() {}
+    public void setupForm() {
+    }
 
     @PostMapping
     public Callable<String> sumbitForm(@RequestParam("courtName") String courtName, Model model) {
         return () -> {
             List<Reservation> reservations = Collections.emptyList();
-            if(courtName != null) {
+            if (courtName != null) {
                 Delayer.randomDelay();  // 서비스를 일부러 지연시킨다.
                 reservations = reservationService.query(courtName);
             }
@@ -41,5 +45,22 @@ public class ReservationQueryController1 {
 
             return "reservationQuery";
         };
+    }
+
+    @GetMapping(params = {"courtName"})
+    public ResponseBodyEmitter find(@RequestParam("courtName") String courtName) {
+        final ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+        taskExecutor.execute(() -> {
+            List<Reservation> reservations = reservationService.query(courtName);
+            try{
+                for(Reservation reservation : reservations) {
+                    emitter.send(reservation);
+                }
+            }catch (IOException e) {
+                emitter.completeWithError(e);
+            }
+        });
+
+        return emitter;
     }
 }
--- ch5-spring-mvc-async/src/main/java/com/yosep/spring/web/controller/ReservationQueryController3.java
+++ ch5-spring-mvc-async/src/main/java/com/yosep/spring/web/controller/ReservationQueryController3.java
@@ -10,13 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Controller
-@RequestMapping("/reservationQuery")
+@RequestMapping("/reservation-query1")
 public class ReservationQueryController3 {
     private final ReservationService reservationService;
     private final TaskExecutor taskExecutor;
@@ -42,5 +44,25 @@ public class ReservationQueryController3 {
             model.addAttribute("reservations", reservations);
             return "reservationQuery";
         }, taskExecutor);
+    }
+
+    @GetMapping(params = {"courtName"})
+    public SseEmitter find(@RequestParam("courtName") String courtName) {
+        final SseEmitter emitter = new SseEmitter();
+
+        taskExecutor.execute(() -> {
+            List<Reservation> reservations = reservationService.query(courtName);
+            try{
+                for(Reservation reservation : reservations) {
+                    Delayer.delay(15);
+                    emitter.send(reservation);
+                }
+                emitter.complete();
+            }catch (IOException e) {
+                emitter.completeWithError(e);
+            }
+        });
+
+        return emitter;
     }
 }
